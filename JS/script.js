dd5938da5852bfa83bf48a5b65eb38021782393a
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-25 17:23:55 +0200

    fixed Timeleft

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,26 @@
 const booleanLesson = new Date('May 26, 2023 09:30:00');
+
 console.log(booleanLesson.getDate());
+
 console.log(booleanLesson.getHours());
+
 console.log(booleanLesson.getMinutes());
+
 console.log(booleanLesson.getSeconds());
 
 const myDay = new Date('May 25, 2023 06:30:10');
+
 console.log(myDay.getDate());
+
 console.log(myDay.getHours());
+
 console.log(myDay.getMinutes());
+
 console.log(myDay.getSeconds());
 
+
 let daysLeft = Math.abs(booleanLesson.getDate() - myDay.getDate() - 1);
+
 console.log(daysLeft);
 
 let hoursLeft = Math.floor((booleanLesson - myDay) / (1000 * 60 * 60)) % 24; // il modulo di 24 mi garantisce che il valore dei minuti rimanga compreso tra 0 e 23 indipendentemente dalla differenza
