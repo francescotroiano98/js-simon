5fda021663263f4aa706462a89f250aa2ad3fc35
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-25 17:23:20 +0200

    Fix TimeLeft

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,25 @@
-
-
-
 const booleanLesson = new Date('May 26, 2023 09:30:00');
 console.log(booleanLesson.getDate());
 console.log(booleanLesson.getHours());
 console.log(booleanLesson.getMinutes());
 console.log(booleanLesson.getSeconds());
 
-const myDay = new Date('May 25, 2023 17:30:00');
+const myDay = new Date('May 25, 2023 06:30:10');
 console.log(myDay.getDate());
 console.log(myDay.getHours());
 console.log(myDay.getMinutes());
 console.log(myDay.getSeconds());
 
 let daysLeft = Math.abs(booleanLesson.getDate() - myDay.getDate() - 1);
-
 console.log(daysLeft);
 
-let hoursLeft = Math.abs(booleanLesson.getHours() - myDay.getHours());
-
+let hoursLeft = Math.floor((booleanLesson - myDay) / (1000 * 60 * 60)) % 24; // il modulo di 24 mi garantisce che il valore dei minuti rimanga compreso tra 0 e 23 indipendentemente dalla differenza
 console.log(hoursLeft);
 
-let minutesLeft = Math.abs(booleanLesson.getMinutes() - myDay.getMinutes());
-
+let minutesLeft = Math.floor((booleanLesson - myDay) / (1000 * 60)) % 60; // il modulo di 60 mi garantisce che il valore dei minuti rimanga compreso tra 0 e 59 indipendentemente dalla differenza
 console.log(minutesLeft);
 
-let secondsLeft = Math.abs(booleanLesson.getSeconds() - myDay.getSeconds());
-
+let secondsLeft = Math.floor((booleanLesson - myDay) / 1000) % 60; // il modulo di 60 mi garantisce che il valore dei secondi rimanga compreso tra 0 e 59 indipendentemente dalla differenza
 console.log(secondsLeft);
 
 let minutes = 30
